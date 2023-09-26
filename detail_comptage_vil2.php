b10146c51cc4f90e2808e09833382e80d18feb5d
sergyoliver <kassoliver1234@gmail.com>
2023-09-26 13:32:18 +0000

    Add files via upload

--- detail_comptage_vil2.php
+++ detail_comptage_vil2.php
@@ -65,6 +65,20 @@ $i=1;
                                     <th>Village</th>
                                     <th>Passage</th>
                                     <th>Parcelle</th>
+                                    <th>Pied</th>
+                                    <th>CLASSE A</th>
+                                    <th>CLASSE B</th>
+                                    <th>CLASSE C</th>
+                                    <th>CLASSE D</th>
+                                    <th>PERTE A</th>
+                                    <th>PERTE B</th>
+                                    <th>FE</th>
+                                    <th>FLO </th>
+                                    <th>NOUE </th>
+                                    <th>OBSERVATIONS </th>
+                                    <th>enregistré le </th>
+                                    <th>Par </th>
+                                    <th>Position X Y </th>
                                     <th>Actions</th>
 
                                 </tr>
@@ -75,13 +89,26 @@ $i=1;
                                 <?php
                                 $i=1;
                                // echo $rowdel['code_delegation'];
-                               //var_dump(array("an" => $camp,"p" =>$pas,"dl" => $rowdel['code_delegation'],"vc" => $rowdelv['code_village']));
-                                 $sqlccacao_dr = $bdd->prepare("SELECT count(id) as a, parcelle_code  from comptage_cacaos WHERE  an_campagne = :an  and id_passage_periode = :p and supp=0 and delegation_code = :dl and  village_code = :vc   group by parcelle_code ");
+                                $sqlccacao_dr = $bdd->prepare("SELECT *  from comptage_cacaos WHERE  an_campagne = :an  and id_passage_periode = :p and supp=0 and delegation_code = :dl  and village_code = :vc   ");
                                 $sqlccacao_dr->execute(array("an" => $camp,"p" =>$pas,"dl" => $rowdel['code_delegation'],"vc" => $rowdelv['code_village']));
 
                                 while($row = $sqlccacao_dr->fetch()) {
-                                   
-                                    $vilc= $rowdelv['code_village'];
+                                    $code_pied = $row['pied_code'];
+
+                                    $sqlp = $bdd->prepare("SELECT *  from pieds WHERE code_pied= :c  ");
+                                    $sqlp->execute(array("c" => $code_pied));
+                                    $rowp = $sqlp->fetch();
+
+
+
+
+                                    $depa = $row['departement_code'];
+                                    $sqldep = $bdd->prepare("SELECT *  from departements WHERE code_departement= :c  ");
+                                    $sqldep->execute(array("c" => $depa));
+                                    $rowdep = $sqldep->fetch();
+
+
+                                    $vilc= $row['village_code'];
                                     if (!empty($vilc)){
                                         $sqlvil = $bdd->prepare("SELECT *  from villages WHERE code_village= :c  ");
                                         $sqlvil->execute(array("c" => $vilc));
@@ -93,7 +120,7 @@ $i=1;
                                     $sqlparcelle = $bdd->prepare("SELECT *  from parcelles WHERE code_parcelle= :c  ");
                                     $sqlparcelle->execute(array("c" => $codepar));
                                     $rowpar = $sqlparcelle->fetch();
-                                    $idpassage = $pas;
+                                    $idpassage = $row['id_passage_periode'];
                                     $rsp = $bdd->prepare("select * from passage_periodes WHERE id = :p  and type_pied='K'  ");
                                     $rsp->execute(array('p'=>$idpassage));
                                     $rowpa = $rsp->fetch();
@@ -105,9 +132,32 @@ $i=1;
                                         <td><?php if (isset( $rowvil['designation']) ) echo $rowvil['designation'];  ?></td>
                                         <td><?php if (isset( $rowpa['libelle']) ) echo $rowpa['libelle'];  ?></td>
                                         <td><?php if (isset($rowpar['nom_parcelle'])) echo $rowpar['nom_parcelle'];  ?></td>
-                                        <td style="text-decoration: underline"><a href="?page=detail_comptage_vil2&idel=<?php echo $rowdel['code_delegation'] ?>&camp=<?php echo $camp ?>&pas=<?php echo $pas ?>&vil=<?php echo $codev ?>"><?php echo number_format($row['a']); ?></a></td>
-
-
+                                        <td><?php echo $rowp['numero_pied'];  ?></td>
+                                        <td><?php echo $row['fruit_a'];  ?></td>
+                                        <td><?php echo $row['fruit_b'];  ?></td>
+                                        <td><?php echo $row['fruit_c'];  ?></td>
+                                        <td><?php echo $row['fruit_d'];  ?></td>
+                                        <td><?php echo $row['pertes_a'];  ?></td>
+                                        <td><?php echo $row['pertes_b'];  ?></td>
+                                        <td><?php echo $row['fe'];  ?></td>
+                                        <td><?php echo $row['flo'];  ?></td>
+                                        <td><?php echo $row['Noue'];  ?></td>
+                                        <td><?php if (isset($row['raison_supp'])) echo $row['raison_supp']  ?></td>
+                                        <td><?php if (isset($row['date_creation'])) echo format_date($row['date_creation']);  ?></td>
+                                        <td><?php if (isset($row['agent_id'])) {
+                                                $sqla = $bdd->prepare("SELECT *  FROM agents WHERE id= :c  ");
+                                                $sqla->execute(array("c" => $row['agent_id']));
+                                                $rowag = $sqla->fetch();
+                                                echo $rowag['matricule'];
+                                            }
+                                            ?></td>
+                                        <td><?php if (isset($row['posX'])) echo $row['posX'] ." ". $row['posY'];  ?></td>
+                                        <td> <a  href="#modif<?php echo $row['id']  ?>"  data-toggle="modal"> <span class="fa fa-pencil" style="font-size: 15px;"></span></a>
+                                            <?php if ($row['valider']==0) {?> &nbsp; &nbsp; <a href="#"> <i class="fa fa-check-square-o" style="font-size: 15px; color: #FF0000"></i></a>
+                                            <?php }else{?>
+                                                <a href="#"> &nbsp; &nbsp;<i class="fa fa-check-square-o"  style="font-size: 15px; color: #16ac11"></i></a>
+                                            <?php    } ?>
+                                        </td>
                                     </tr>
 
                                     <?php $i++; } ?>
